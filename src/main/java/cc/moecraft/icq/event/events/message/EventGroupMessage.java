aa8a0e638c2092d67c72ff237ff6a93f86c96d4b
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:33:20 -0400

    [+] 群聊消息事件

---
+++ src/main/java/cc/moecraft/icq/event/events/message/EventGroupMessage.java
@@ -0,0 +1,35 @@
+package cc.moecraft.icq.event.events.message;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class EventGroupMessage extends EventMessage
+{
+    @SerializedName("anonymous")
+    @Expose
+    public String anonymous;
+
+    @SerializedName("anonymous_flag")
+    @Expose
+    public String anonymousFlag;
+
+    @SerializedName("group_id")
+    @Expose
+    public Long groupId;
+
+    @SerializedName("sub_type")
+    @Expose
+    public String subType;
+}
