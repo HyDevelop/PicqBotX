d6f45c6f7577777981e2ec24922558414e723d52
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:33:41 -0400

    [+] 讨论组聊天事件

---
+++ src/main/java/cc/moecraft/icq/event/events/message/EventDiscussMessage.java
@@ -0,0 +1,23 @@
+package cc.moecraft.icq.event.events.message;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class EventDiscussMessage extends EventMessage
+{
+    @SerializedName("discuss_id")
+    @Expose
+    public Long discussId;
+}
