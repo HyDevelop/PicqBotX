6415897be04912c1621ee38a0cf8b8a17d9ea59d
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:33:55 -0400

    [+] ICQ监听器抽象类

---
+++ src/main/java/cc/moecraft/icq/event/IcqListener.java
@@ -0,0 +1,14 @@
+package cc.moecraft.icq.event;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public abstract class IcqListener
+{
+
+}
