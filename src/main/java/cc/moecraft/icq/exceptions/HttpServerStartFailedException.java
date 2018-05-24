4bd5483218cca198641b065e3a869d5094a3d258
Hykilpikonna <admin@moecraft.cc>
2018-05-24 15:40:56 -0400

    [+] 添加启动失败异常

---
+++ src/main/java/cc/moecraft/icq/exceptions/HttpServerStartFailedException.java
@@ -0,0 +1,37 @@
+package cc.moecraft.icq.exceptions;
+
+import cc.moecraft.logger.DebugLogger;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data @AllArgsConstructor
+public class HttpServerStartFailedException extends Exception
+{
+    private DebugLogger logger;
+    private Exception subException;
+
+    @Override
+    public void printStackTrace()
+    {
+        if (subException.getMessage().toLowerCase().contains("address already in use:"))
+        {
+            logger.error("端口已经被占用了... 是不是写错了?");
+            logger.error("默认的31091是发送, 31092是接收哦!");
+        }
+        else
+        {
+            super.printStackTrace();
+            subException.printStackTrace();
+        }
+    }
+}
