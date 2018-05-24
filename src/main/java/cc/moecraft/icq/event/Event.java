8f8f04e77d542525631bc03f31ea3cff0cd3a67f
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:31:34 -0400

    [+] 事件

---
+++ src/main/java/cc/moecraft/icq/event/Event.java
@@ -0,0 +1,18 @@
+package cc.moecraft.icq.event;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public abstract class Event
+{
+}
