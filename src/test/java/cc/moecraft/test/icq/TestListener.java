a1fc06650502c1143994bb2ed7d744436877a739
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:37:47 -0400

    [T] 测试机器人添加测试监听器

---
+++ src/test/java/cc/moecraft/test/icq/TestListener.java
@@ -0,0 +1,22 @@
+package cc.moecraft.test.icq;
+
+import cc.moecraft.icq.event.EventHandler;
+import cc.moecraft.icq.event.IcqListener;
+import cc.moecraft.icq.event.events.message.EventPrivateMessage;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class TestListener extends IcqListener
+{
+    @EventHandler
+    public void onPMEvent(EventPrivateMessage event)
+    {
+        System.out.println("接到消息");
+    }
+}
