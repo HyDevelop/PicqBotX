6eb5d3c717ae2377f65cda88bdc2d1ac8eaf91f4
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:32:39 -0400

    [+] 事件监听器注解

---
+++ src/main/java/cc/moecraft/icq/event/EventHandler.java
@@ -0,0 +1,17 @@
+package cc.moecraft.icq.event;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EventHandler
+{
+}
