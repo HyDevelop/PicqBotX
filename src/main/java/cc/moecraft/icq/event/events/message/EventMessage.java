16edbfdf2613dbc89e6a78aac2c329d9df18b269
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:32:59 -0400

    [+] 消息事件

---
+++ src/main/java/cc/moecraft/icq/event/events/message/EventMessage.java
@@ -0,0 +1,52 @@
+package cc.moecraft.icq.event.events.message;
+
+import cc.moecraft.icq.event.Event;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public abstract class EventMessage extends Event
+{
+    @SerializedName("message_type")
+    @Expose
+    public String messageType;
+
+    @SerializedName("font")
+    @Expose
+    public Long font;
+
+    @SerializedName("message")
+    @Expose
+    public String message;
+
+    @SerializedName("message_id")
+    @Expose
+    public Long messageId;
+
+    @SerializedName("raw_message")
+    @Expose
+    public String rawMessage;
+
+    @SerializedName("self_id")
+    @Expose
+    public Long selfId;
+
+    @SerializedName("time")
+    @Expose
+    public Long time;
+
+    @SerializedName("user_id")
+    @Expose
+    public Long userId;
+}
