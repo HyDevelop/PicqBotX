d37ecffca5aa33ea6e72de6b6e6b67b091a527a6
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:34:43 -0400

    [+] 已注册的方法类添加执行方法的方法

--- src/main/java/cc/moecraft/icq/event/RegisteredListenerMethod.java
+++ src/main/java/cc/moecraft/icq/event/RegisteredListenerMethod.java
@@ -19,4 +19,17 @@ public class RegisteredListenerMethod
 {
     private Method method;
     private IcqListener listener;
+
+    /**
+     * 反射执行监听方法
+     * @param event 事件
+     * @return Json响应
+     * @throws InvocationTargetException 反射失败
+     * @throws IllegalAccessException 无访问权限 (不可能发生)
+     */
+    public String call(Event event) throws InvocationTargetException, IllegalAccessException 
+    {
+        method.setAccessible(true);
+        return (String) method.invoke(listener, event);
+    }
 }
