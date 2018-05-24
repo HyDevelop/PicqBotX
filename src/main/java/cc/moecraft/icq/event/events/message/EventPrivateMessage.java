208504e47b2462e6862e7f5f415bbe9cb6213ab0
Hykilpikonna <admin@moecraft.cc>
2018-05-24 18:33:11 -0400

    [+] 私聊消息事件

---
+++ src/main/java/cc/moecraft/icq/event/events/message/EventPrivateMessage.java
@@ -0,0 +1,23 @@
+package cc.moecraft.icq.event.events.message;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/24 创建!
+ * Created by Hykilpikonna on 2018/05/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class EventPrivateMessage extends EventMessage
+{
+    @SerializedName("sub_type")
+    @Expose
+    public String subType;
+}
