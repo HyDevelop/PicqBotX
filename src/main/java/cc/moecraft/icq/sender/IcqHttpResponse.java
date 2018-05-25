ef1c19acad5de2127bdb9993d17cb11dd6b981ea
Hykilpikonna <admin@moecraft.cc>
2018-05-25 07:06:06 -0400

    [+] 添加HTTP响应数据类

---
+++ src/main/java/cc/moecraft/icq/sender/IcqHttpResponse.java
@@ -0,0 +1,23 @@
+package cc.moecraft.icq.sender;
+
+import lombok.Data;
+
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/31/24 创建!
+ * Created by Hykilpikonna on 2018/31/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class IcqHttpResponse
+{
+    private int returnCode;
+
+    private String status;
+
+    private Map<String, Object> data;
+}
