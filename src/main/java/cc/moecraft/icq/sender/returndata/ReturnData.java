14f1a5c628e5536793a737036c67f99de2cf1295
Hykilpikonna <admin@moecraft.cc>
2018-05-25 14:39:55 -0400

    [+] 处理后的返回数据类

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/ReturnData.java
@@ -0,0 +1,20 @@
+package cc.moecraft.icq.sender.returndata;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/25 创建!
+ * Created by Hykilpikonna on 2018/05/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class ReturnData<T extends ReturnPojoBase>
+{
+    public T data;
+    public Long returnCode;
+    public ReturnStatus status;
+}
