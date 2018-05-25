ad1bcefc8581d207df1f587ee4db33b1c2116892
Hykilpikonna <admin@moecraft.cc>
2018-05-25 14:38:53 -0400

    [+] 未处理的响应数据

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/RawReturnData.java
@@ -0,0 +1,35 @@
+package cc.moecraft.icq.sender.returndata;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/25 创建!
+ * Created by Hykilpikonna on 2018/05/25!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class RawReturnData
+{
+    @SerializedName("data")
+    @Expose
+    public JsonElement data;
+
+    @SerializedName("retcode")
+    @Expose
+    public Long returnCode;
+
+    @SerializedName("status")
+    @Expose
+    public ReturnStatus status;
+}
