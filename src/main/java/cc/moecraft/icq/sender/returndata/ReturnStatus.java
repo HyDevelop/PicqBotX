ae2b94a7f60eb73e7e317b2f0603110a0857324f
Hykilpikonna <admin@moecraft.cc>
2018-05-25 14:40:53 -0400

    [+] 返回状态enum

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/ReturnStatus.java
@@ -0,0 +1,16 @@
+package cc.moecraft.icq.sender.returndata;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/25 创建!
+ * Created by Hykilpikonna on 2018/05/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public enum ReturnStatus
+{
+    ok,     // 成功
+    async,  // 异步
+    failed  // 失败
+}
