e5f1d16d01ab0f8daa2e8e718e67123e9d0b5fa1
Hykilpikonna <admin@moecraft.cc>
2018-05-25 14:40:08 -0400

    [+] 处理后的返回数据List类

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/ReturnListData.java
@@ -0,0 +1,24 @@
+package cc.moecraft.icq.sender.returndata;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/25 创建!
+ * Created by Hykilpikonna on 2018/05/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class ReturnListData<T extends ReturnPojoBase>
+{
+    public List<T> data;
+    public Long returnCode;
+    public ReturnStatus status;
+}
