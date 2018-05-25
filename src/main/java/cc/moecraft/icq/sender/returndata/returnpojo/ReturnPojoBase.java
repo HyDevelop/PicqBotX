03cee32836c0d387f4b672bd3ac15493abaa09a3
Hykilpikonna <admin@moecraft.cc>
2018-05-25 14:39:21 -0400

    [+] 响应POJO父类

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/ReturnPojoBase.java
@@ -0,0 +1,13 @@
+package cc.moecraft.icq.sender.returndata.returnpojo;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/25 创建!
+ * Created by Hykilpikonna on 2018/05/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public abstract class ReturnPojoBase
+{
+}
