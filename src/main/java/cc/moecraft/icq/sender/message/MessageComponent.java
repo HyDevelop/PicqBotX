f8fb7f3716f6e08e0faab0575c3b21430bb2887e
Hykilpikonna <admin@moecraft.cc>
2018-05-26 13:30:17 -0400

    [+] 消息组件

---
+++ src/main/java/cc/moecraft/icq/sender/message/MessageComponent.java
@@ -0,0 +1,14 @@
+package cc.moecraft.icq.sender.message;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public abstract class MessageComponent
+{
+    public abstract String toString();
+}
