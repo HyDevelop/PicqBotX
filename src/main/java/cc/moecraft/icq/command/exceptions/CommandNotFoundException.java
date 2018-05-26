90c7ef714056d46501adf901af088129ee69cab7
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:05:54 -0400

    [+] 未知指令异常

---
+++ src/main/java/cc/moecraft/icq/command/exceptions/CommandNotFoundException.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.command.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/05 创建!
+ * Created by Hykilpikonna on 2018/05/05!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class CommandNotFoundException extends Exception { }
