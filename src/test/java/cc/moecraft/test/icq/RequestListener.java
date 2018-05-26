ae19a62704f4bcc8c8c4596f75f08737c1ffadc0
Hykilpikonna <admin@moecraft.cc>
2018-05-26 10:45:43 -0400

    [T] 更新测试

--- src/test/java/cc/moecraft/test/icq/RequestListener.java
+++ src/test/java/cc/moecraft/test/icq/RequestListener.java
@@ -19,13 +19,13 @@ public class RequestListener extends IcqListener
     // 接受好友请求的字符 (只要contains就行) 虽然知道有只要输一个字母就能过的bug但是还是这样写了...
     private static final String acceptedFriendRequestMessage = "hykilpikonna 小桂 me@hydev.org hydev";
     // 接受加群申请的字符
-    private static final String acceptedGroupRequestMessage = "hyosuircbot iosu hyconfiglib hylogger osu机器人";
+    private static final String acceptedGroupRequestMessage = "hyosuircbot iosu hyconfiglib hylogger osu机器人 hyultimateplugin languageapi hytranscript";
 
     @EventHandler
     public void onFriendRequest(EventFriendRequest event)
     {
         // 因为我这里设置的问题是 "开发我的人叫什么?" 所以答案这样判断...
-        String answer = event.getMessage().replace("问题1:开发我的人叫什么?\n回答:", "");
+        String answer = event.getComment().replace("问题1:开发我的人叫什么?\n回答:", "");
         if (acceptedFriendRequestMessage.contains(answer.toLowerCase()))
         {
             event.accept();
@@ -41,5 +41,19 @@ public class RequestListener extends IcqListener
     {
         // 接受所有群邀请
         event.accept();
+    }
+
+    @EventHandler
+    public void onGroupAdd(EventGroupAddRequest event)
+    {
+        String answer = event.getComment().replace("问题：填任意一个HyDEV的项目\n答案：", "");
+        if (acceptedGroupRequestMessage.contains(answer.toLowerCase()))
+        {
+            event.accept();
+        }
+        else
+        {
+            event.reject("\"" + answer + "\"不是HyDEV的项目哦! 再试试吧!"); // TODO: 把这些输错的人记录下来
+        }
     }
 }
