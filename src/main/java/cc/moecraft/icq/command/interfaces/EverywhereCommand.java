f9f15d355b48dc7812c6169a315ffb22143f6ec5
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:06:56 -0400

    [+] 全局指令

---
+++ src/main/java/cc/moecraft/icq/command/interfaces/EverywhereCommand.java
@@ -0,0 +1,29 @@
+package cc.moecraft.icq.command.interfaces;
+
+import cc.moecraft.icq.event.events.message.EventDiscussMessage;
+import cc.moecraft.icq.event.events.message.EventMessage;
+import cc.moecraft.icq.user.Group;
+import cc.moecraft.icq.user.User;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public interface EverywhereCommand extends IcqCommand
+{
+    /**
+     * 执行指令
+     * @param event 事件
+     * @param sender 发送者的用户
+     * @param command 指令名 ( 不包含指令参数 )
+     * @param args 指令参数 ( 不包含指令名 )
+     * @return 发送回去的消息 ( 当然也可以手动发送然后返回空 )
+     */
+    public String run(EventMessage event, User sender, String command, ArrayList<String> args);
+}
