f7948ea4892cdccd43df7455e06672e210f7bde2
Hykilpikonna <admin@moecraft.cc>
2018-05-26 07:04:54 -0400

    [+] 获取好友列表响应POJO

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/get/RFriendList.java
@@ -0,0 +1,50 @@
+package cc.moecraft.icq.sender.returndata.returnpojo.get;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/25 创建!
+ * Created by Hykilpikonna on 2018/05/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class RFriendList extends ReturnPojoBase
+{
+    @SerializedName("friend_group_id")
+    @Expose
+    public Long friendGroupId;
+
+    @SerializedName("friend_group_name")
+    @Expose
+    public String friendGroupName;
+
+    @SerializedName("friends")
+    @Expose
+    public List<Friend> friends = null;
+
+    @Data
+    public class Friend
+    {
+        @SerializedName("nickname")
+        @Expose
+        public String nickname;
+
+        @SerializedName("remark")
+        @Expose
+        public String remark;
+
+        @SerializedName("user_id")
+        @Expose
+        public Long userId;
+    }
+}
