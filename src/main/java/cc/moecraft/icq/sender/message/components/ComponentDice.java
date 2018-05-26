1ff534597c74808da95df562e0934ba38df1ca08
Hykilpikonna <admin@moecraft.cc>
2018-05-26 18:46:14 -0400

    [+] 封装所有CQ码

---
+++ src/main/java/cc/moecraft/icq/sender/message/components/ComponentDice.java
@@ -0,0 +1,20 @@
+package cc.moecraft.icq.sender.message.components;
+
+import cc.moecraft.icq.sender.message.MessageComponent;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class ComponentDice extends MessageComponent
+{
+    @Override
+    public String toString()
+    {
+        return "[CQ:dice,type=1]";
+    }
+}
---
+++ src/main/java/cc/moecraft/icq/sender/message/components/ComponentShare.java
@@ -0,0 +1,26 @@
+package cc.moecraft.icq.sender.message.components;
+
+import cc.moecraft.icq.sender.message.MessageComponent;
+import lombok.AllArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+public class ComponentShare extends MessageComponent
+{
+    public String url;
+    public String title;
+    public String content;
+    public String image;
+
+    @Override
+    public String toString() {
+        return String.format("[CQ:share,url=%s,title=%s,content=%s,image=%s]", url, title, content, image);
+    }
+}
