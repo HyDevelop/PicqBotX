5c5d26ad6f8c7b25be840edaf79ac45d8ec40f19
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:20:03 -0400

    [+] 加好友Notice

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/EventNoticeFriendAdd.java
@@ -0,0 +1,12 @@
+package cc.moecraft.icq.event.events.notice;
+
+import cc.moecraft.icq.event.events.notice.groupmember.increase.EventNoticeGroupMemberIncrease;
+
+/**
+ * 成功加好友事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeFriendAdd extends EventNotice
+{
+}
