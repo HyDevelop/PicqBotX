4c2d85bdd71782a1b1aa65b917ecf7360f78ce50
Hykilpikonna <admin@moecraft.cc>
2018-05-26 11:19:31 -0400

    [+] 用户对象缓存管理器

---
+++ src/main/java/cc/moecraft/icq/user/UserManager.java
@@ -0,0 +1,33 @@
+package cc.moecraft.icq.user;
+
+import cc.moecraft.icq.PicqBotX;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@RequiredArgsConstructor
+public class UserManager
+{
+    private final PicqBotX bot;
+    public Map<Long, User> userCache = new HashMap<>();
+
+    /**
+     * 用ID获取User
+     * @param id QQ号
+     * @return User对象
+     */
+    public User getUserFromID(long id)
+    {
+        if (userCache.containsKey(id)) return userCache.get(id);
+        return userCache.put(id, new User(bot, id));
+    }
+}
