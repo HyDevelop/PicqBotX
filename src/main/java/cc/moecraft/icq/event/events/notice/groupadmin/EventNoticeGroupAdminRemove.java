c72685e5aefb95ce72e990cb3fd53acfafeedf21
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:21:04 -0400

    [+] 移除管理员Notice

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupadmin/EventNoticeGroupAdminRemove.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.event.events.notice.groupadmin;
+
+/**
+ * 群员取消管理员事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupAdminRemove extends EventNoticeGroupAdminChange
+{
+
+}
