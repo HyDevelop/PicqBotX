788ff5813226422a162482ef1838ba5f5b2ff986
Hykilpikonna <admin@moecraft.cc>
2018-05-26 07:05:29 -0400

    [+] 获取版本信息响应POJO

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/get/RVersionInfo.java
@@ -0,0 +1,37 @@
+package cc.moecraft.icq.sender.returndata.returnpojo.get;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 版本信息
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class RVersionInfo extends ReturnPojoBase
+{
+    @SerializedName("coolq_directory")
+    @Expose
+    public String coolqDirectory;
+
+    @SerializedName("coolq_edition")
+    @Expose
+    public String coolqEdition;
+
+    @SerializedName("plugin_build_configuration")
+    @Expose
+    public String pluginBuildConfiguration;
+
+    @SerializedName("plugin_build_number")
+    @Expose
+    public Long pluginBuildNumber;
+
+    @SerializedName("plugin_version")
+    @Expose
+    public String pluginVersion;
+}
