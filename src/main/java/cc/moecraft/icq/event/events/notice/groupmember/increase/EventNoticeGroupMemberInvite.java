acc14f68e05ea333477d2332a889971558e27b55
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:23:24 -0400

    [+] 群员邀请加群Notice

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupmember/increase/EventNoticeGroupMemberInvite.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.event.events.notice.groupmember.increase;
+
+/**
+ * 群员邀请加群事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupMemberInvite extends EventNoticeGroupMemberIncrease
+{
+
+}
