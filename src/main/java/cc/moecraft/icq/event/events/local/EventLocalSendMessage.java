aaef0de5e34ba76c3885029017cee6c8728a6155
Hykilpikonna <admin@moecraft.cc>
2018-05-26 09:40:57 -0400

    [+] 向外发送消息事件

---
+++ src/main/java/cc/moecraft/icq/event/events/local/EventLocalSendMessage.java
@@ -0,0 +1,22 @@
+package cc.moecraft.icq.event.events.local;
+
+import cc.moecraft.icq.event.Event;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 从机器人发送消息事件
+ * 这里的消息可以改的, 所有监听器执行完之后才会发送
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data @RequiredArgsConstructor @AllArgsConstructor
+public class EventLocalSendMessage extends Event
+{
+    public final long id;
+    public final String message;
+    public boolean autoEscape = false;
+}
