a3128b57cc5facb158000d5d54fa81e172f2668f
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:09:33 -0400

    [T] 更新测试Main

--- src/test/java/cc/moecraft/test/icq/TestBot.java
+++ src/test/java/cc/moecraft/test/icq/TestBot.java
@@ -16,18 +16,25 @@ public class TestBot
 {
     public static void main(String[] args)
     {
+        // 创建机器人对象 ( 信息发送URL, 发送端口, 接收端口, 是否DEBUG )
         PicqBotX bot = new PicqBotX("127.0.0.1", 31091, 31092, false);
+
         try
         {
             bot.getEventManager()
-                    .registerListener(new TestListener())
-                    .registerListener(new RequestListener());
-            if (!bot.isDebug()) bot.getEventManager().registerListener(new SimpleTextLoggingListener());
-            bot.startBot();
+                    .registerListener(new TestListener()) // 注册监听器
+                    .registerListener(new RequestListener()); // 可以注册多个监听器
+            if (!bot.isDebug()) bot.getEventManager().registerListener(new SimpleTextLoggingListener()); // 这个只是在不开Debug的时候用来Log消息的
+
+            // 启用指令管理器, 启用的时候会自动注册指令
+            // 这些字符串是指令前缀, 比如!help的前缀就是!
+            bot.enableCommandManager("bot -", "!", "/", "~");
+
+            bot.startBot(); // 启动机器人
         }
-        catch (HttpServerStartFailedException | VersionIncorrectException e)
+        catch (HttpServerStartFailedException | VersionIncorrectException | IllegalAccessException | InstantiationException e)
         {
-            e.printStackTrace();
+            e.printStackTrace(); // 启动失败, 结束程序
         }
     }
 }
