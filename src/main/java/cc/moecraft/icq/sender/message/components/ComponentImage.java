b231bd0a706285f1d7ebc0b913c78cf0207c4fcd
Hykilpikonna <admin@moecraft.cc>
2018-05-26 13:30:38 -0400

    [+] 图片组件

---
+++ src/main/java/cc/moecraft/icq/sender/message/components/ComponentImage.java
@@ -0,0 +1,24 @@
+package cc.moecraft.icq.sender.message.components;
+
+import cc.moecraft.icq.sender.message.MessageComponent;
+import lombok.AllArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+public class ComponentImage extends MessageComponent
+{
+    public String fileOrURL;
+
+    @Override
+    public String toString()
+    {
+        return "[CQ:image,file=" + fileOrURL + "]";
+    }
+}
