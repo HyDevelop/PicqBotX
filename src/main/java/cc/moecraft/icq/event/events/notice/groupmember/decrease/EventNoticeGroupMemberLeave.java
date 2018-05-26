bf914c3dd53071348c15d1184ba79a6535c3dcb3
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:22:55 -0400

    [+] 群员主动退群Notice

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupmember/decrease/EventNoticeGroupMemberLeave.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.event.events.notice.groupmember.decrease;
+
+/**
+ * 群员退群事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupMemberLeave extends EventNoticeGroupMemberDecrease
+{
+
+}
