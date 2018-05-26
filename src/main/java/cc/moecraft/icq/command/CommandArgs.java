db1310ed6da4ba525f6fbefeeff8d41e94cc1149
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:07:51 -0400

    [+] 指令参数

---
+++ src/main/java/cc/moecraft/icq/command/CommandArgs.java
@@ -0,0 +1,64 @@
+package cc.moecraft.icq.command;
+
+import cc.moecraft.icq.command.exceptions.CommandNotFoundException;
+import cc.moecraft.icq.command.exceptions.NotACommandException;
+import cc.moecraft.icq.command.interfaces.IcqCommand;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/05 创建!
+ * Created by Hykilpikonna on 2018/05/05!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data @AllArgsConstructor
+public class CommandArgs
+{
+    private String prefix;
+    private String commandName;
+    private IcqCommand commandRunner;
+    private ArrayList<String> args;
+
+    /**
+     * 从字符串消息转换为CommandArgs
+     * @param fullCommand 完整字符串消息
+     * @param isChannel 是不是从频道发出来的
+     * @return CommandArgs指令
+     */
+    public static CommandArgs parse(CommandManager commandManager, String fullCommand, boolean isChannel) throws NotACommandException, CommandNotFoundException
+    {
+        String prefix = getPrefix(commandManager, fullCommand);
+
+        if (prefix.equals("") && isChannel) throw new NotACommandException(); // 不是指令
+
+        String command = fullCommand.replace(prefix, "").toLowerCase(); // "echo"
+        fullCommand = fullCommand.replace(command, ""); // ["hi", "there"]
+
+        ArrayList<String> args = new ArrayList<>(Arrays.asList(fullCommand.split(" "))); // String "!ecHO hi" -> ArrayList ["!ecHO", "hi", "there"]
+
+        if (!commandManager.getRegisteredCommands().containsKey(command)) throw new CommandNotFoundException(); // 无法找到指令
+
+        IcqCommand commandToRun = commandManager.getRegisteredCommands().get(command);
+
+        return new CommandArgs(prefix, command, commandToRun, args);
+    }
+
+    /**
+     * 获取指令前缀
+     * @param text 消息
+     * @return 是指令的话返回指令前缀, 不是指令的话返回""
+     */
+    private static String getPrefix(CommandManager commandManager, String text)
+    {
+        for (String prefix : commandManager.getPrefixes())
+            if (text.startsWith(prefix)) return prefix;
+
+        return "";
+    }
+}
