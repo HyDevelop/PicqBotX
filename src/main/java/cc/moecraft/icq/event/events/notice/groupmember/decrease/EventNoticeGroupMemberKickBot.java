85fb88b7b87b4ec86c535c7a3647571729a5c638
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:22:41 -0400

    [+] 机器人号被踢事件

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupmember/decrease/EventNoticeGroupMemberKickBot.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.event.events.notice.groupmember.decrease;
+
+/**
+ * 自己被踢了_(:з」∠)_
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupMemberKickBot extends EventNoticeGroupMemberDecrease
+{
+
+}
