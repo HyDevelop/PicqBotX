73901fed9267c0403bd7e1fbb285a1242d9bec60
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:06:49 -0400

    [+] 讨论组指令

---
+++ src/main/java/cc/moecraft/icq/command/interfaces/DiscussCommand.java
@@ -0,0 +1,27 @@
+package cc.moecraft.icq.command.interfaces;
+
+import cc.moecraft.icq.event.events.message.EventDiscussMessage;
+import cc.moecraft.icq.user.Group;
+import cc.moecraft.icq.user.GroupUser;
+import cc.moecraft.icq.user.User;
+
+import java.util.ArrayList;
+
+/**
+ * 讨论组指令
+ *
+ * @author Hykilpikonna
+ */
+public interface DiscussCommand extends IcqCommand
+{
+    /**
+     * 执行指令
+     * @param event 事件
+     * @param sender 发送者的用户
+     * @param discuss 讨论组
+     * @param command 指令名 ( 不包含指令参数 )
+     * @param args 指令参数 ( 不包含指令名 )
+     * @return 发送回去的消息 ( 当然也可以手动发送然后返回空 )
+     */
+    public String discussMessage(EventDiscussMessage event, GroupUser sender, Group discuss, String command, ArrayList<String> args);
+}
