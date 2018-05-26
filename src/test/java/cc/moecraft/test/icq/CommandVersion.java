a151bc0aa3638dbc795549055f5ad784ebb33743
Hykilpikonna <admin@moecraft.cc>
2018-05-26 19:09:01 -0400

    [+] 指令创建教程

--- src/test/java/cc/moecraft/test/icq/CommandVersion.java
+++ src/test/java/cc/moecraft/test/icq/CommandVersion.java
@@ -15,17 +15,23 @@ import java.util.ArrayList;
  *
  * @author Hykilpikonna
  */
-public class CommandVersion implements EverywhereCommand
+public class CommandVersion implements EverywhereCommand // 继承EverywhereCommand就是无论私聊还是群聊都能收到的指令
 {
-    @Override
-    public String run(EventMessage event, User sender, String command, ArrayList<String> args)
-    {
-        return "TestBot - PicqBotX v1.0.1";
-    }
-
+    // 指令属性
     @Override
     public CommandProperties properties()
     {
-        return new CommandProperties("version", "v");
+        // 这个括号里填指令名和其他名称, 指令名必须至少有一个
+        // 这个的话, 用"!v", "!version", 和"!版本"都能触发指令 (感叹号为你设置的前缀, 不一定必须要感叹号)
+        return new CommandProperties("version", "v", "版本");
+    }
+
+    // 机器人接到指令后会执行这个方法 ( 实现不同的接口的话方法名不一定一样 )
+    @Override
+    public String run(EventMessage event, User sender, String command, ArrayList<String> args)
+    {
+        // 处理, 返回值会自动回复回去
+        // 这里因为这个指令是用来查版本的, 所以直接返回字符串了
+        return "TestBot - PicqBotX v1.0.1";
     }
 }
