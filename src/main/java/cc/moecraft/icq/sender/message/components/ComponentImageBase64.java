c8a0e59e3bb125c9fbdcaeb8afcc808de4410c5c
Hykilpikonna <admin@moecraft.cc>
2018-05-26 13:31:02 -0400

    [+] Base64图片组件

---
+++ src/main/java/cc/moecraft/icq/sender/message/components/ComponentImageBase64.java
@@ -0,0 +1,25 @@
+package cc.moecraft.icq.sender.message.components;
+
+import cc.moecraft.icq.sender.message.MessageBuilder;
+import cc.moecraft.icq.sender.message.MessageComponent;
+import lombok.AllArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+public class ComponentImageBase64 extends MessageComponent
+{
+    public String base64;
+
+    @Override
+    public String toString()
+    {
+        return "[CQ:image,file=base64://" + base64 + "]";
+    }
+}
