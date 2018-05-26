90ad5b6a722e1ed4283fa5b7c4fe83fe04fe5833
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:06:24 -0400

    [+] ICQ指令接口

---
+++ src/main/java/cc/moecraft/icq/command/interfaces/IcqCommand.java
@@ -0,0 +1,18 @@
+package cc.moecraft.icq.command.interfaces;
+
+import cc.moecraft.icq.command.CommandProperties;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public interface IcqCommand
+{
+    public CommandProperties properties();
+}
