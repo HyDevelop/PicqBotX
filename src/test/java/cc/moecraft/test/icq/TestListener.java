693afb24be538a9270157223a56e59958747c91a
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:39:23 -0400

    [T] 测试信息返回

--- src/test/java/cc/moecraft/test/icq/TestListener.java
+++ src/test/java/cc/moecraft/test/icq/TestListener.java
@@ -1,10 +1,15 @@
 package cc.moecraft.test.icq;
 
+import cc.moecraft.icq.PicqBotX;
 import cc.moecraft.icq.event.EventHandler;
 import cc.moecraft.icq.event.IcqListener;
 import cc.moecraft.icq.event.events.message.EventPrivateMessage;
 import cc.moecraft.icq.sender.IcqHttpApi;
-import cc.moecraft.icq.sender.IcqHttpResponse;
+import cc.moecraft.icq.sender.returndata.RawReturnData;
+import cc.moecraft.icq.sender.returndata.ReturnData;
+import cc.moecraft.icq.sender.returndata.ReturnListData;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RGroupList;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RLoginInfo;
 import com.google.gson.JsonElement;
 
 /**
@@ -20,15 +25,39 @@ public class TestListener extends IcqListener
     @EventHandler
     public void onPMEvent(EventPrivateMessage event)
     {
-        System.out.println("接到消息");
+        // System.out.println("接到消息");
 
         // 纯属测试没有嘲讽意思啦...
         if (event.getMessage().equals("你以为这是yangjinhe/maintain-robot?"))
+        {
             event.respond("其实是我Hykilpikonna/PicqBotX哒!");
+        }
 
-        JsonElement response = event.getBot().getHttpApi().send(IcqHttpApi.SEND_PRIVATE_MSG,
-                "user_id", 565656,
-                "message", "hi",
-                "auto_escape", false);
+        if (event.getMessage().equals("测试给小桂发Hi"))
+        {
+            JsonElement response = event.getBot().getHttpApi().send(IcqHttpApi.SEND_PRIVATE_MSG,
+                    "user_id", 565656,
+                    "message", "hi",
+                    "auto_escape", false);
+        }
+
+        if (event.getMessage().equals("测试回复数据"))
+        {
+            testDataReturn(event.getBot());
+        }
+    }
+
+    /**
+     * 用来测试回复数据的
+     */
+    public void testDataReturn(PicqBotX bot)
+    {
+        // 测试成功 #1: 普通泛型
+        // ReturnData<RLoginInfo> returnData = bot.getHttpApi().getLoginInfo();
+        // bot.getLogger().log("Return Data = " + returnData);
+
+        // 测试 #2: List泛型
+        ReturnListData<RGroupList> returnListData = bot.getHttpApi().getGroupList();
+        bot.getLogger().log("Return List Data = " + returnListData);
     }
 }
