8b33e2c0fed99791b21fe1b7061f5ef2e92d3aae
Hykilpikonna <admin@moecraft.cc>
2018-05-26 08:11:56 -0400

    [+] 添加版本检测

--- src/main/java/cc/moecraft/icq/PicqBotX.java
+++ src/main/java/cc/moecraft/icq/PicqBotX.java
@@ -2,11 +2,14 @@ package cc.moecraft.icq;
 
 import cc.moecraft.icq.event.EventManager;
 import cc.moecraft.icq.exceptions.HttpServerStartFailedException;
+import cc.moecraft.icq.exceptions.VersionIncorrectException;
+import cc.moecraft.icq.exceptions.VersionRecommendException;
 import cc.moecraft.icq.sender.IcqHttpApi;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RVersionInfo;
+import cc.moecraft.logger.AnsiColor;
 import cc.moecraft.logger.DebugLogger;
 import lombok.Getter;
-
-import java.io.PipedReader;
+import lombok.Setter;
 
 /**
  * 此类由 Hykilpikonna 在 2018/05/24 创建!
@@ -24,6 +27,9 @@ public class PicqBotX
     @Getter
     private boolean debug;
 
+    @Getter @Setter
+    private boolean noVerify = false; // 这个为true是跳过版本验证, 需要手动设置
+
     @Getter
     private EventManager eventManager;
 
@@ -36,14 +42,35 @@ public class PicqBotX
     public PicqBotX(String postUrl, int postPort, int socketPort, boolean debug)
     {
         setDebug(debug);
+        httpApi = new IcqHttpApi(postUrl, postPort);
         httpServer = new HttpServer(socketPort, this);
         eventManager = new EventManager(this);
-        httpApi = new IcqHttpApi(postUrl, postPort);
     }
 
-    public void startBot() throws HttpServerStartFailedException
+    /**
+     * 启动机器人
+     * @throws HttpServerStartFailedException HTTP服务器启动失败
+     * @throws VersionIncorrectException 版本错误
+     */
+    public void startBot() throws HttpServerStartFailedException, VersionIncorrectException
     {
-        httpServer.start();
+        try
+        {
+            verifyHttpPluginVersion();
+            logger.log(AnsiColor.GREEN + "检测版本正确, 正在启动...");
+            httpServer.start();
+        }
+        catch (VersionIncorrectException e)
+        {
+            logger.error("HTTP插件版本不正确, 已停止启动" +
+                    "\n- 当前版本: " + e.getCurrentVersion() +
+                    "\n- 需要的版本: " + e.getRequiredVersion());
+            throw e;
+        }
+        catch (VersionRecommendException e)
+        {
+            logger.error("版本正确, 不过用酷Q Pro的话效果更好哦!");
+        }
     }
 
     /**
@@ -54,5 +81,22 @@ public class PicqBotX
     {
         this.debug = debug;
         logger.setDebug(debug);
+    }
+
+    /**
+     * 验证HTTP插件版本
+     * @throws VersionIncorrectException 版本不对
+     */
+    public void verifyHttpPluginVersion() throws VersionIncorrectException, VersionRecommendException
+    {
+        if (noVerify) return;
+
+        RVersionInfo versionInfo = httpApi.getVersionInfo().getData();
+
+        if (!versionInfo.getPluginVersion().startsWith("4."))
+            throw new VersionIncorrectException("4.*", versionInfo.getPluginVersion());
+
+        if (!versionInfo.getCoolqEdition().equalsIgnoreCase("pro"))
+            throw new VersionRecommendException();
     }
 }
