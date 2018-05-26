83198c866a548645f141fe141d822ea2f2d7052d
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:22:29 -0400

    [+] 群员被踢事件

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupmember/decrease/EventNoticeGroupMemberKick.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.event.events.notice.groupmember.decrease;
+
+/**
+ * 群员被踢事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupMemberKick extends EventNoticeGroupMemberDecrease
+{
+
+}
