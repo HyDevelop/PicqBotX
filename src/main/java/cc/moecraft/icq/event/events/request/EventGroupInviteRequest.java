1beb093c3b8a61caae979c3ac1b021923bbee47d
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:24:35 -0400

    [+] 群邀请请求事件

---
+++ src/main/java/cc/moecraft/icq/event/events/request/EventGroupInviteRequest.java
@@ -0,0 +1,36 @@
+package cc.moecraft.icq.event.events.request;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 群邀请事件 ( 别人邀请机器人的时候 )
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class EventGroupInviteRequest extends EventRequest
+{
+    @SerializedName("group_id")
+    @Expose
+    public Long groupId;
+
+    @SerializedName("sub_type")
+    @Expose
+    public String subType;
+
+    @Override
+    public void accept()
+    {
+        getBot().getHttpApi().approveGroupRequest(flag, subType); // TODO: 测试
+    }
+
+    @Override
+    public void reject(String reason)
+    {
+        getBot().getHttpApi().rejectGroupRequest(flag, subType, reason);
+    }
+}
