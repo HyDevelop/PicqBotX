1f31f634472938cb88660cbc49b958304d81bd18
Hykilpikonna <admin@moecraft.cc>
2018-05-26 18:46:29 -0400

    [+] 添加测试Filter

---
+++ src/test/java/cc/moecraft/test/icq/TestFilter.java
@@ -0,0 +1,30 @@
+package cc.moecraft.test.icq;
+
+import cc.moecraft.icq.event.EventHandler;
+import cc.moecraft.icq.event.IcqListener;
+import cc.moecraft.icq.event.events.local.EventLocalSendMessage;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class TestFilter extends IcqListener
+{
+    @EventHandler
+    public void onAllLocalMessageEvent(EventLocalSendMessage event) // 监听所有发送消息的事件
+    {
+        // 获取消息
+        String message = event.getMessage();
+
+        // 这里可以做任何处理
+        // 我把所有"%prefix%"变量替换成了"!"
+        message = message.replace("%prefix%", "!");
+
+        // 设置消息
+        event.setMessage(message);
+    }
+}
