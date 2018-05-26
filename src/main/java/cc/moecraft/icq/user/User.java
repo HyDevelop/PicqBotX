25e0b90a2985de03cf94b9aa0aa31afa41c01ab1
Hykilpikonna <admin@moecraft.cc>
2018-05-26 11:19:20 -0400

    [+] 用户类

---
+++ src/main/java/cc/moecraft/icq/user/User.java
@@ -0,0 +1,44 @@
+package cc.moecraft.icq.user;
+
+import cc.moecraft.icq.PicqBotX;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RStrangerInfo;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@RequiredArgsConstructor
+public class User
+{
+    private final PicqBotX bot;
+    @Getter
+    public final long id;
+
+    public RStrangerInfo info;
+
+    /**
+     * 获取数据
+     * @return 数据 (不一定是最新)
+     */
+    public RStrangerInfo getInfo()
+    {
+        if (info != null) return info;
+        return refreshInfo(false);
+    }
+
+    /**
+     * 更新数据
+     * @param noCache 是否不用缓存
+     * @return 更新的数据
+     */
+    public RStrangerInfo refreshInfo(boolean noCache)
+    {
+        return info = bot.getHttpApi().getStrangerInfo(id, noCache).getData();
+    }
+}
