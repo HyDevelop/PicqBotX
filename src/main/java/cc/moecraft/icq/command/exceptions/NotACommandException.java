c85f28d89a434b791700334de9119112ff577c81
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:06:02 -0400

    [+] 不是指令异常

---
+++ src/main/java/cc/moecraft/icq/command/exceptions/NotACommandException.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.command.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/05 创建!
+ * Created by Hykilpikonna on 2018/05/05!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class NotACommandException extends Exception { }
