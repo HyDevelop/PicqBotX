b82399c4bec0cfc688debb473cb5ba932950f842
Hykilpikonna <admin@moecraft.cc>
2018-05-26 08:10:29 -0400

    [+] 版本信息不对异常

---
+++ src/main/java/cc/moecraft/icq/exceptions/VersionIncorrectException.java
@@ -0,0 +1,22 @@
+package cc.moecraft.icq.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/26 创建!
+ * Created by Hykilpikonna on 2018/05/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data @AllArgsConstructor
+public class VersionIncorrectException extends Exception
+{
+    public String requiredVersion;
+    public String currentVersion;
+}
