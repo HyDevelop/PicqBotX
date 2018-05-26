b3872ed95710c3978466b90ca4f9b19f4e6fd759
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:21:13 -0400

    [+] 设置管理员Notice

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupadmin/EventNoticeGroupAdminSet.java
@@ -0,0 +1,11 @@
+package cc.moecraft.icq.event.events.notice.groupadmin;
+
+/**
+ * 群员成为管理员事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupAdminSet extends EventNoticeGroupAdminChange
+{
+
+}
