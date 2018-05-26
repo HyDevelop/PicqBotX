ba6b2d2c197ce14240fc381fe6a34901ab6428a1
Hykilpikonna <admin@moecraft.cc>
2018-05-26 09:41:31 -0400

    [+] 向外发送私聊消息事件

---
+++ src/main/java/cc/moecraft/icq/event/events/local/EventLocalSendPrivateMessage.java
@@ -0,0 +1,13 @@
+package cc.moecraft.icq.event.events.local;
+
+/**
+ * 机器人发送私聊消息事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventLocalSendPrivateMessage extends EventLocalSendMessage
+{
+    public EventLocalSendPrivateMessage(long id, String message, boolean auto_escape) {
+        super(id, message, auto_escape);
+    }
+}
