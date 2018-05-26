3dfad03cbbb14bbf3669faa15f0cec5745f1c95d
Hykilpikonna <admin@moecraft.cc>
2018-05-25 21:23:07 -0400

    [+] 群员加群Notice

---
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupmember/increase/EventNoticeGroupMemberApprove.java
@@ -0,0 +1,13 @@
+package cc.moecraft.icq.event.events.notice.groupmember.increase;
+
+import cc.moecraft.icq.event.events.notice.groupmember.decrease.EventNoticeGroupMemberDecrease;
+
+/**
+ * 群员加群事件
+ *
+ * @author Hykilpikonna
+ */
+public class EventNoticeGroupMemberApprove extends EventNoticeGroupMemberIncrease
+{
+
+}
