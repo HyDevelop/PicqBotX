e762fc671612aaaec6839dcd5456f8f8c40a7e5a
Hykilpikonna <admin@moecraft.cc>
2018-05-26 16:06:40 -0400

    [+] 私聊指令

---
+++ src/main/java/cc/moecraft/icq/command/interfaces/PrivateCommand.java
@@ -0,0 +1,24 @@
+package cc.moecraft.icq.command.interfaces;
+
+import cc.moecraft.icq.event.events.message.EventPrivateMessage;
+import cc.moecraft.icq.user.User;
+
+import java.util.ArrayList;
+
+/**
+ * 私聊指令
+ *
+ * @author Hykilpikonna
+ */
+public interface PrivateCommand extends IcqCommand
+{
+    /**
+     * 执行指令
+     * @param event 事件
+     * @param sender 发送者的用户
+     * @param command 指令名 ( 不包含指令参数 )
+     * @param args 指令参数 ( 不包含指令名 )
+     * @return 发送回去的消息 ( 当然也可以手动发送然后返回空 )
+     */
+    public String privateMessage(EventPrivateMessage event, User sender, String command, ArrayList<String> args);
+}
