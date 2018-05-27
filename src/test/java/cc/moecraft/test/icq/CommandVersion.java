550341d2754c1d5f2fb85fadcede2deb94694940
Hykilpikonna <admin@moecraft.cc>
2018-05-27 10:21:00 -0400

    [+] 版本指令添加状态信息

--- src/test/java/cc/moecraft/test/icq/CommandVersion.java
+++ src/test/java/cc/moecraft/test/icq/CommandVersion.java
@@ -3,6 +3,10 @@ package cc.moecraft.test.icq;
 import cc.moecraft.icq.command.CommandProperties;
 import cc.moecraft.icq.command.interfaces.EverywhereCommand;
 import cc.moecraft.icq.event.events.message.EventMessage;
+import cc.moecraft.icq.sender.message.MessageBuilder;
+import cc.moecraft.icq.sender.returndata.ReturnData;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RStatus;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RVersionInfo;
 import cc.moecraft.icq.user.User;
 
 import java.util.ArrayList;
@@ -31,7 +35,17 @@ public class CommandVersion implements EverywhereCommand // 继承EverywhereComm
     public String run(EventMessage event, User sender, String command, ArrayList<String> args)
     {
         // 处理, 返回值会自动回复回去
+
+        // 获取版本信息
+        RVersionInfo versionInfo = event.getHttpApi().getVersionInfo().getData();
+        RStatus status = event.getHttpApi().getStatus().getData();
+
         // 这里因为这个指令是用来查版本的, 所以直接返回字符串了
-        return "TestBot - PicqBotX v1.0.1.4";
+        return new MessageBuilder()
+                .add("TestBot - PicqBotX v1.0.1 b4").newLine()
+                .add("- 酷Q ").add(versionInfo.getCoolqEdition()).add(" HTTP插件: ")
+                    .add(versionInfo.getPluginVersion()).add(" b").add(versionInfo.getPluginBuildNumber()).newLine()
+                .add("- 运行状态: Good: ").add(status.getGood()).add(", ").add(status.getPluginsGood())
+                .toString();
     }
 }
