a40c4c78f6632b98f0e0e93d29b8a448b34340d9
Hykilpikonna <admin@moecraft.cc>
2018-06-14 08:25:40 +0800

    [+] 酷Q工具类

---
+++ src/main/java/cc/moecraft/icq/utils/CQUtils.java
@@ -0,0 +1,21 @@
+package cc.moecraft.icq.utils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/06/13 创建!
+ * Created by Hykilpikonna on 2018/06/13!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class CQUtils
+{
+    public static String decodeMessage(String rawMessage)
+    {
+        return rawMessage
+                .replace("&amp;", "&")
+                .replace("&#91;", "[")
+                .replace("&#93;", "]")
+                .replace("&#44;", ",");
+    }
+}
