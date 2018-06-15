3c028fd7e977c8a6db917d1a013521ea82677baf
Hykilpikonna <admin@moecraft.cc>
2018-06-16 03:52:36 +0800

    [+] 字符串转码JUnit测试类

---
+++ src/test/java/cc/moecraft/test/general/CodecTest.java
@@ -0,0 +1,28 @@
+package cc.moecraft.test.general;
+
+import cc.moecraft.icq.utils.StringCodecUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/06/15 创建!
+ * Created by Hykilpikonna on 2018/06/15!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class CodecTest
+{
+    @Test
+    public void testAscii()
+    {
+        String original = "abc123 45中文";
+        String ascii = StringCodecUtils.toAscii(original);
+        String restored = StringCodecUtils.fromAscii(ascii);
+
+        System.out.println(ascii);
+        System.out.println(restored);
+
+        if (!original.equals(restored)) throw new RuntimeException();
+    }
+}
