f73f1cdefae5726b48141d0736902ca28cde8078
Hykilpikonna <admin@moecraft.cc>
2018-07-12 11:06:04 +0800

    [+] 添加抽象EventGroupOrDiscussMessage

---
+++ src/main/java/cc/moecraft/icq/event/events/message/EventGroupOrDiscussMessage.java
@@ -0,0 +1,35 @@
+package cc.moecraft.icq.event.events.message;
+
+import cc.moecraft.icq.user.Group;
+import cc.moecraft.icq.user.GroupUser;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/11 创建!
+ * Created by Hykilpikonna on 2018/07/11!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public abstract class EventGroupOrDiscussMessage extends EventMessage
+{
+    /**
+     * 获取群用户对象
+     * @return 发送消息的群用户
+     */
+    public GroupUser getGroupSender()
+    {
+        return getBot().getGroupUserManager().getUserFromID(senderId, getGroup());
+    }
+
+    /**
+     * 获取群对象
+     * @return 群对象
+     */
+    public abstract Group getGroup();
+
+    /**
+     * 踢掉这个用户
+     */
+    public abstract void kick();
+}
