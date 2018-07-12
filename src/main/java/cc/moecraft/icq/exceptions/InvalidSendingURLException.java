0372cdf52eecd83f772cc83cc60f09a4c484d71c
Hykilpikonna <admin@moecraft.cc>
2018-07-12 11:05:13 +0800

    [+] 发送URL错误异常

---
+++ src/main/java/cc/moecraft/icq/exceptions/InvalidSendingURLException.java
@@ -0,0 +1,14 @@
+package cc.moecraft.icq.exceptions;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/11 创建!
+ * Created by Hykilpikonna on 2018/07/11!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class InvalidSendingURLException extends Exception
+{
+
+}
