d05fd9691ac6ec2c9f5b9149c2dc9cb0d6c9833b
Hykilpikonna <admin@moecraft.cc>
2018-07-20 12:01:17 +0800

    [+] 添加RGroupDetail类

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/get/RGroupDetail.java
@@ -0,0 +1,77 @@
+package cc.moecraft.icq.sender.returndata.returnpojo.get;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/20 创建!
+ * Created by Hykilpikonna on 2018/07/20!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class RGroupDetail extends ReturnPojoBase
+{
+    @SerializedName("admin_count")
+    @Expose
+    private Long adminCount;
+
+    @SerializedName("admins")
+    @Expose
+    private List<Admin> admins = null;
+
+    @SerializedName("category")
+    @Expose
+    private Long category;
+
+    @SerializedName("create_time")
+    @Expose
+    private Long createTime;
+
+    @SerializedName("group_id")
+    @Expose
+    private Long groupId;
+
+    @SerializedName("group_name")
+    @Expose
+    private String groupName;
+
+    @SerializedName("introduction")
+    @Expose
+    private String introduction;
+
+    @SerializedName("max_admin_count")
+    @Expose
+    private Long maxAdminCount;
+
+    @SerializedName("max_member_count")
+    @Expose
+    private Long maxMemberCount;
+
+    @SerializedName("member_count")
+    @Expose
+    private Long memberCount;
+
+    @SerializedName("owner_id")
+    @Expose
+    private Long ownerId;
+
+    public static class Admin
+    {
+        @SerializedName("nickname")
+        @Expose
+        private String nickname;
+
+        @SerializedName("role")
+        @Expose
+        private String role;
+
+        @SerializedName("user_id")
+        @Expose
+        private Long userId;
+    }
+}
