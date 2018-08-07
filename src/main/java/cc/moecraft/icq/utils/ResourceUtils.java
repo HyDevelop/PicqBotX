c105ee41ab5ce697f03d44f523f56578cc8d3144
Hykilpikonna <admin@moecraft.cc>
2018-08-07 13:26:40 +0800

    [+] 类资源工具类

---
+++ src/main/java/cc/moecraft/icq/utils/ResourceUtils.java
@@ -0,0 +1,42 @@
+package cc.moecraft.icq.utils;
+
+import cc.moecraft.logger.HyLogger;
+import cc.moecraft.logger.format.AnsiFormat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/27 创建!
+ * Created by Hykilpikonna on 2018/07/27!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class ResourceUtils
+{
+    public static void logResource(Class resourceClass, HyLogger logger, String name, String... variablesAndReplacements)
+    {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(resourceClass.getClassLoader().getResourceAsStream(name))))
+        {
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                for (int i = 0; i < variablesAndReplacements.length; i += 2)
+                {
+                    line = line.replace("%{" + variablesAndReplacements[i] + "}", variablesAndReplacements[i + 1]);
+                }
+
+                line = AnsiFormat.replaceAllFormatWithANSI(line);
+
+                logger.log(line);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
