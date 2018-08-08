3cd28f2da9986972a1a054f5b77e451315640dfc
Hykilpikonna <admin@moecraft.cc>
2018-08-08 21:56:20 +0800

    [+] 实现多线程异步处理指令

--- src/main/java/cc/moecraft/icq/command/CommandListener.java
+++ src/main/java/cc/moecraft/icq/command/CommandListener.java
@@ -1,11 +1,14 @@
 package cc.moecraft.icq.command;
 
+import cc.moecraft.icq.event.Event;
 import cc.moecraft.icq.event.EventHandler;
 import cc.moecraft.icq.event.IcqListener;
 import cc.moecraft.icq.event.events.message.EventDiscussMessage;
 import cc.moecraft.icq.event.events.message.EventGroupMessage;
+import cc.moecraft.icq.event.events.message.EventMessage;
 import cc.moecraft.icq.event.events.message.EventPrivateMessage;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 /**
  * 此类由 Hykilpikonna 在 2018/05/26 创建!
@@ -23,18 +26,45 @@ public class CommandListener extends IcqListener
     @EventHandler
     public void onPrivateMessage(EventPrivateMessage event)
     {
-        commandManager.runCommand(event);
+        run(event);
     }
 
     @EventHandler
     public void onGroupMessage(EventGroupMessage event)
     {
-        commandManager.runCommand(event);
+        run(event);
     }
 
     @EventHandler
     public void onDiscussMessage(EventDiscussMessage event)
     {
-        commandManager.runCommand(event);
+        run(event);
+    }
+
+    private void run(EventMessage event)
+    {
+        CommandRunnable runnable = new CommandRunnable(event);
+
+        if (event.getBot().isUseAsync()) runnable.runAsync();
+        else runnable.run();
+    }
+
+    @RequiredArgsConstructor
+    private class CommandRunnable implements Runnable
+    {
+        private Thread thread;
+        private final EventMessage event;
+
+        @Override
+        public void run()
+        {
+            commandManager.runCommand(event);
+        }
+
+        public void runAsync()
+        {
+            thread = new Thread(this, "Thread-" + System.currentTimeMillis());
+            thread.start();
+        }
     }
 }
