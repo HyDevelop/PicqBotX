66f00f933d624f1407520bb946ef371681c89e5e
Hykilpikonna <admin@moecraft.cc>
2018-08-08 22:01:57 +0800

    [U] 版本 v1.3.4.428

--- src/main/java/cc/moecraft/icq/PicqBotX.java
+++ src/main/java/cc/moecraft/icq/PicqBotX.java
@@ -74,7 +74,7 @@ public class PicqBotX
     @Getter @Setter
     private boolean useAsync = false; // 是否异步
 
-    public static final String VERSION = "1.3.4.426";
+    public static final String VERSION = "1.3.4.428";
 
     public PicqBotX(String postUrl, int postPort, int socketPort, boolean debug)
     {
