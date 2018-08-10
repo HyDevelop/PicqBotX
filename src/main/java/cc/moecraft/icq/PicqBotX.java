a8b29d3d9e4f87da9f70781e255efc31e435ed5a
Hykilpikonna <admin@moecraft.cc>
2018-08-10 11:02:54 +0800

    [D] PicqBotX.java:233: 警告: java.lang.InstantiationException没有 @throws

--- src/main/java/cc/moecraft/icq/PicqBotX.java
+++ src/main/java/cc/moecraft/icq/PicqBotX.java
@@ -230,6 +230,8 @@ public class PicqBotX
     /**
      * 启用指令系统
      * @param prefixes 前缀
+     * @throws InstantiationException 反射失败
+     * @throws IllegalAccessException 反射失败
      */
     public void enableCommandManager(String ... prefixes) throws InstantiationException, IllegalAccessException
     {
