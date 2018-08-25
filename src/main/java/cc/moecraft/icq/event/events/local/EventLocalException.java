f071db46cd3be29bd68a642bcbbb6d68287cf55a
Hykilpikonna <admin@moecraft.cc>
2018-08-25 15:41:45 -0400

    [+] 报错本地事件

---
+++ src/main/java/cc/moecraft/icq/event/events/local/EventLocalException.java
@@ -0,0 +1,21 @@
+package cc.moecraft.icq.event.events.local;
+
+import cc.moecraft.icq.event.Event;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/24 创建!
+ * Created by Hykilpikonna on 2018/08/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class EventLocalException extends EventLocal
+{
+    private final Throwable exception;
+    private final Event parentEvent;
+}
