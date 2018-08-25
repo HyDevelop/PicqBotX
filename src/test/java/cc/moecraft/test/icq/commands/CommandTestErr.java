939c7dbc5bbd7efade655315a54858c671377e21
Hykilpikonna <admin@moecraft.cc>
2018-08-25 15:49:31 -0400

    [T] 测试报错输出

---
+++ src/test/java/cc/moecraft/test/icq/commands/CommandTestErr.java
@@ -0,0 +1,31 @@
+package cc.moecraft.test.icq.commands;
+
+import cc.moecraft.icq.command.CommandProperties;
+import cc.moecraft.icq.command.interfaces.EverywhereCommand;
+import cc.moecraft.icq.event.events.message.EventMessage;
+import cc.moecraft.icq.user.User;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/24 创建!
+ * Created by Hykilpikonna on 2018/08/24!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class CommandTestErr implements EverywhereCommand
+{
+    @Override
+    public String run(EventMessage event, User sender, String command, ArrayList<String> args)
+    {
+        return String.valueOf(1/0);
+    }
+
+    @Override
+    public CommandProperties properties()
+    {
+        return new CommandProperties("test-divide-zero");
+    }
+}
