e8044f87d03152b77c48aa69f63ba0b60a3374f7
Hykilpikonna <admin@moecraft.cc>
2018-08-25 15:38:13 -0400

    [+] 账号管理器

---
+++ src/main/java/cc/moecraft/icq/accounts/AccountManager.java
@@ -0,0 +1,24 @@
+package cc.moecraft.icq.accounts;
+
+import cc.moecraft.icq.event.events.local.EventLocalSendGroupMessage;
+import cc.moecraft.icq.event.events.message.EventGroupMessage;
+import cc.moecraft.icq.sender.IcqHttpApi;
+import cc.moecraft.icq.sender.returndata.returnpojo.get.RGroup;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/25 创建!
+ * Created by Hykilpikonna on 2018/08/25!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class AccountManager
+{
+}
