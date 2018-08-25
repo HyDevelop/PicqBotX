8aeee0aaff294d05261e1f696a0bc4ab8e8d9da6
Hykilpikonna <admin@moecraft.cc>
2018-08-25 15:46:19 -0400

    [+] 记录消息监听器

---
+++ src/main/java/cc/moecraft/icq/accounts/AccountManagerListener.java
@@ -0,0 +1,27 @@
+package cc.moecraft.icq.accounts;
+
+import cc.moecraft.icq.event.EventHandler;
+import cc.moecraft.icq.event.IcqListener;
+import cc.moecraft.icq.event.events.local.EventLocalSendGroupMessage;
+import cc.moecraft.icq.event.events.message.EventGroupMessage;
+import lombok.AllArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/25 创建!
+ * Created by Hykilpikonna on 2018/08/25!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+public class AccountManagerListener extends IcqListener
+{
+    private AccountManager accountManager;
+
+    @EventHandler
+    public void onGroupMessage(EventLocalSendGroupMessage event)
+    {
+        accountManager.recordMessage(event);
+    }
+}
