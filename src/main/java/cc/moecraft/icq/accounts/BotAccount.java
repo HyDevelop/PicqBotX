38f804e2e662400503435cea49ac62ccc389d4e5
Hykilpikonna <admin@moecraft.cc>
2018-08-25 15:39:26 -0400

    [+] 机器人账号类

---
+++ src/main/java/cc/moecraft/icq/accounts/BotAccount.java
@@ -0,0 +1,34 @@
+package cc.moecraft.icq.accounts;
+
+import cc.moecraft.icq.event.EventManager;
+import cc.moecraft.icq.sender.IcqHttpApi;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/25 创建!
+ * Created by Hykilpikonna on 2018/08/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class BotAccount
+{
+    private final String name;
+    private final String postUrl;
+    private final int postPort;
+
+    @Getter
+    protected long id;
+
+    public BotAccount(String name, EventManager eventManager, String postUrl, int postPort)
+    {
+        this.name = name;
+        this.postUrl = postUrl;
+        this.postPort = postPort;
+
+    }
+}
