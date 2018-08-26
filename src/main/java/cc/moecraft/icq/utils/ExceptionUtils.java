2ff30b74d966b402442925b3e6074bf6906004e2
Hykilpikonna <admin@moecraft.cc>
2018-08-26 15:54:34 -0400

    [+] 异常工具类

---
+++ src/main/java/cc/moecraft/icq/utils/ExceptionUtils.java
@@ -0,0 +1,15 @@
+package cc.moecraft.icq.utils;
+
+import java.lang.reflect.Field;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/26 创建!
+ * Created by Hykilpikonna on 2018/08/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class ExceptionUtils
+{
+}
