1851185ff208cbebd97a32c5b90a3725f66feca4
Hykilpikonna <admin@moecraft.cc>
2018-08-26 19:06:07 -0400

    [+] HyExp消息过滤监听器

---
+++ src/main/java/cc/moecraft/icq/listeners/HyExpressionListener.java
@@ -0,0 +1,24 @@
+package cc.moecraft.icq.listeners;
+
+import cc.moecraft.icq.event.EventHandler;
+import cc.moecraft.icq.event.IcqListener;
+import cc.moecraft.icq.event.events.local.EventLocalSendMessage;
+import cc.moecraft.utils.HyExpressionResolver;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/26 创建!
+ * Created by Hykilpikonna on 2018/08/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class HyExpressionListener extends IcqListener
+{
+    @EventHandler
+    public void onMessageSendEvent(EventLocalSendMessage event)
+    {
+        if (event.getBot().isUniversalHyExpSupport())
+            event.setMessage(HyExpressionResolver.resolve(event.getMessage()));
+    }
+}
