1d252d6a30d6e0f03c9c4826d1397b29bca774b1
Hykilpikonna <admin@moecraft.cc>
2018-08-26 15:54:05 -0400

    [+] 线程工具类

---
+++ src/main/java/cc/moecraft/icq/utils/ThreadUtils.java
@@ -0,0 +1,13 @@
+package cc.moecraft.icq.utils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/08/26 创建!
+ * Created by Hykilpikonna on 2018/08/26!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class ThreadUtils
+{
+}
