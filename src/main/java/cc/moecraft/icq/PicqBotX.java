d69d09823e8211ef7f1a6178e9ba6438382ff74a
Hykilpikonna <admin@moecraft.cc>
2018-09-02 20:46:55 -0400

    [U] 版本 v2.4.2.547

--- src/main/java/cc/moecraft/icq/PicqBotX.java
+++ src/main/java/cc/moecraft/icq/PicqBotX.java
@@ -84,7 +84,7 @@ public class PicqBotX
     @Getter @Setter
     private boolean universalHyExpSupport = false; // 是否全局替换HyExp表达式
 
-    public static final String VERSION = "2.4.2.541";
+    public static final String VERSION = "2.4.2.547";
 
     /**
      * 构造器
