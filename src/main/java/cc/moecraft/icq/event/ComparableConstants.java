f968aaac810c22a01f9b386a0fdd348d0660f0f5
Hykilpikonna <admin@moecraft.cc>
2018-09-02 20:44:39 -0400

    [+] 添加检测时间范围常量

---
+++ src/main/java/cc/moecraft/icq/event/ComparableConstants.java
@@ -0,0 +1,14 @@
+package cc.moecraft.icq.event;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/09/02 创建!
+ * Created by Hykilpikonna on 2018/09/02!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class ComparableConstants
+{
+    public static int TimeDetectionRangeInSeconds = 2;
+}
