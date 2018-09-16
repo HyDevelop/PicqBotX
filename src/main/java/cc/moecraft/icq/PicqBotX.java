7c5370f113c8737473dde38c94ced8bdb78aaf7e
Hykilpikonna <admin@moecraft.cc>
2018-09-16 11:56:13 -0400

    [U] 版本 v2.4.5.566

--- src/main/java/cc/moecraft/icq/PicqBotX.java
+++ src/main/java/cc/moecraft/icq/PicqBotX.java
@@ -138,7 +138,7 @@ public class PicqBotX
     /**
      * 当前版本
      */
-    public static final String VERSION = "2.4.3.552";
+    public static final String VERSION = "2.4.5.566";
 
     /**
      * 构造器
