56ebaa65ac7d851664308ab02c7240fad000ae39
Hykilpikonna <admin@moecraft.cc>
2018-09-16 11:52:05 -0400

    [O] 优化导入

--- src/main/java/cc/moecraft/icq/event/events/notice/groupadmin/EventNoticeGroupAdminChange.java
+++ src/main/java/cc/moecraft/icq/event/events/notice/groupadmin/EventNoticeGroupAdminChange.java
@@ -4,8 +4,6 @@ import cc.moecraft.icq.accounts.BotAccount;
 import cc.moecraft.icq.event.ContentComparable;
 import cc.moecraft.icq.event.events.notice.EventNotice;
 import cc.moecraft.icq.event.methodsets.GroupEventMethods;
-import cc.moecraft.icq.sender.returndata.ReturnData;
-import cc.moecraft.icq.sender.returndata.returnpojo.send.RMessageReturnData;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import lombok.*;
