00850d7c63dfa00ac18c79bdbdf7e45e5e4ccfab
Hykilpikonna <admin@moecraft.cc>
2019-03-21 18:54:22 -0400

    [+] Create constant class.

---
+++ src/main/java/cc/moecraft/icq/PicqConstants.java
@@ -0,0 +1,16 @@
+package cc.moecraft.icq;
+
+/**
+ * The class {@code PicqConstants} is a class with static and final
+ * constant fields. They can be changed with reflection.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-21!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-21 18:45
+ */
+public class PicqConstants
+{
+}
