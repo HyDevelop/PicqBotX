b43aa546b1a2bcdf77f491d1235f33edd9a8d732
Hykilpikonna <admin@moecraft.cc>
2019-03-21 20:02:43 -0400

    [M] Move logInitDone() method to MiscUtils class.

---
+++ src/main/java/cc/moecraft/icq/utils/MiscUtils.java
@@ -0,0 +1,43 @@
+package cc.moecraft.icq.utils;
+
+import cc.moecraft.logger.HyLogger;
+
+import static cc.moecraft.logger.format.AnsiColor.*;
+import static cc.moecraft.logger.format.AnsiColor.YELLOW;
+
+/**
+ * The class {@code MiscUtils} is an utilities class for random things.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-21!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-21 19:58
+ */
+public class MiscUtils
+{
+    /**
+     * 输出一行"初始化完成"的日志
+     *
+     * @param logger 日志对象
+     * @param name 要输出的名字
+     * @param greens 绿色星号数量
+     * @param reds 红色星号数量
+     */
+    public static void logInitDone(HyLogger logger, String name, int greens, int reds)
+    {
+        StringBuilder greenStars = new StringBuilder();
+        StringBuilder redStars = new StringBuilder();
+
+        for (int i = 0; i < greens; i++) greenStars.append("*");
+        for (int i = 0; i < reds; i++) redStars.append("*");
+
+        logger.log(String.format("%s%s%s初始化完成%s [%s%s%s%s%s] ...(%s ms)",
+                YELLOW, name, GREEN, YELLOW,
+                GREEN, greenStars.toString(), RED, redStars.toString(), YELLOW,
+                Math.round(logger.timing.getMilliseconds() * 100d) / 100d));
+
+        logger.timing.reset();
+    }
+}
