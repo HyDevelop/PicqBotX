7c4377cfa4847040d1e675b49336140d340227e6
Hykilpikonna <admin@moecraft.cc>
2019-03-21 20:05:55 -0400

    [O] Use VerifyFailedException instead of System.exit(1)

---
+++ src/main/java/cc/moecraft/icq/exceptions/VerifyFailedException.java
@@ -0,0 +1,15 @@
+package cc.moecraft.icq.exceptions;
+
+/**
+ * Thrown when HTTP version verification is not successful.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-21!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-21 20:03
+ */
+public class VerifyFailedException extends RuntimeException
+{
+}
