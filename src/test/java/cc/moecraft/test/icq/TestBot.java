1d1dce50f00d66c8af7928e5482ab4aafd6cb3c2
Hykilpikonna <admin@moecraft.cc>
2019-03-21 20:35:12 -0400

    [O] Refactor TestBot

--- src/test/java/cc/moecraft/test/icq/TestBot.java
+++ src/test/java/cc/moecraft/test/icq/TestBot.java
@@ -1,7 +1,6 @@
 package cc.moecraft.test.icq;
 
 import cc.moecraft.icq.PicqBotX;
-import cc.moecraft.icq.exceptions.HttpServerStartFailedException;
 import cc.moecraft.test.icq.features.annoy.AnnoyingListener;
 import cc.moecraft.test.icq.features.antirecall.AntiRecallListener;
 import cc.moecraft.test.icq.listeners.ExceptionListener;
@@ -10,52 +9,51 @@ import cc.moecraft.test.icq.listeners.SimpleTextLoggingListener;
 import cc.moecraft.test.icq.listeners.TestListener;
 
 /**
- * 此类由 Hykilpikonna 在 2018/05/24 创建!
- * Created by Hykilpikonna on 2018/05/24!
- * Github: https://github.com/hykilpikonna
- * QQ: admin@moecraft.cc -OR- 871674895
+ * 功能测试机器人!
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-21!
  *
- * @author Hykilpikonna
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-21 20:26
  */
 public class TestBot
 {
     public static void main(String[] args)
     {
-        // 创建机器人对象 ( 信息发送URL, 发送端口, 接收端口, 是否DEBUG )
-        //PicqBotX bot = new PicqBotX(31092, false);
-        PicqBotX bot = new PicqBotX("127.0.0.1", 31090, 31092, false);
+        // 创建机器人对象 ( 接收端口 )
+        PicqBotX bot = new PicqBotX(31092);
 
-        //bot.getAccountManager().addAccount(
-                //new BotAccount("One", bot.getEventManager(), "127.0.0.1", 31091),
-                //new BotAccount("Two", bot.getEventManager(), "127.0.0.1", 31090)
-        //);
+        // 添加一个机器人账户 ( 名字, 发送URL, 发送端口 )
+        bot.addAccount("Bot01", "127.0.0.1", 31091);
 
-        bot.setMaintenanceMode(false);
+        // 启用HyExp ( 非必要 )
         bot.setUniversalHyExpSupport(true);
 
         // 设置异步
-        bot.setUseAsync(true);
+        bot.getConfig().setUseAsync(true);
 
-        try
-        {
-            bot.getEventManager()
-                    .registerListener(new TestListener()) // 注册监听器
-                    .registerListener(new RequestListener())
-                    .registerListener(new AntiRecallListener())
-                    .registerListener(new AnnoyingListener())
-                    .registerListener(new ExceptionListener()); // 可以注册多个监听器
-            if (!bot.isDebug()) bot.getEventManager().registerListener(new SimpleTextLoggingListener()); // 这个只是在不开Debug的时候用来Log消息的
+        // 注册事件
+        bot.getEventManager().registerListeners(
+                new TestListener(),
+                new RequestListener(),
+                new AntiRecallListener(),
+                new AnnoyingListener(),
+                new ExceptionListener()); // 可以注册多个监听器
 
-            // 启用指令管理器, 启用的时候会自动注册指令
-            // 这些字符串是指令前缀, 比如!help的前缀就是!
-            bot.enableCommandManager("bot -", "!", "/", "~", "！", "我以令咒命之，", "我以令咒命之, ");
-            System.out.println(bot.getCommandManager().getRegisteredCommands());
+        // 在没有Debug的时候加上这个消息日志监听器
+        if (!bot.getConfig().isDebug())
+            bot.getEventManager().registerListener(new SimpleTextLoggingListener());
 
-            bot.startBot(); // 启动机器人
-        }
-        catch (HttpServerStartFailedException | IllegalAccessException | InstantiationException e)
-        {
-            e.printStackTrace(); // 启动失败, 结束程序
-        }
+        // 启用指令管理器, 启用的时候会自动注册指令
+        // 这些字符串是指令前缀, 比如指令"!help"的前缀就是"!"
+        bot.enableCommandManager("bot -", "!", "/", "~", "！", "我以令咒命之，", "我以令咒命之, ", "test -");
+
+        // Debug输出所有已注册的指令
+        bot.getLogger().debug(bot.getCommandManager().getRegisteredCommands().toString());
+
+        // 启动机器人
+        bot.startBot();
     }
 }
