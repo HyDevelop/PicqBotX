61eb8708d75a73dd2401a1a6387e363a68f7876b
Hykilpikonna <admin@moecraft.cc>
2019-03-23 11:57:07 -0400

    [F] Fix RCredentials inheritence

--- src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/get/RCredentials.java
+++ src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/get/RCredentials.java
@@ -1,16 +1,22 @@
 package cc.moecraft.icq.sender.returndata.returnpojo.get;
 
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
 
 /**
  * 获取Credentials
  *
  * @author Hykilpikonna
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class RCredentials
+@Setter(AccessLevel.NONE)
+public class RCredentials extends ReturnPojoBase
 {
     @SerializedName("cookies")
     @Expose
