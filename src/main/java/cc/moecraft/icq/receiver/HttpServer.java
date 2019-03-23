4c34403d0270ff5058fff80f5cb9c36e6f154dfb
Hykilpikonna <admin@moecraft.cc>
2019-03-23 13:07:01 -0400

    [U] Make the interface an abstract class instead

--- src/main/java/cc/moecraft/icq/receiver/HttpServer.java
+++ src/main/java/cc/moecraft/icq/receiver/HttpServer.java
@@ -1,5 +1,7 @@
 package cc.moecraft.icq.receiver;
 
+import cc.moecraft.icq.exceptions.HttpServerException;
+
 /**
  * The class {@code HttpServerInterface} is a http server to receive and
  * parse the package sent from the http plugin of CoolQ.
@@ -11,7 +13,14 @@ package cc.moecraft.icq.receiver;
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2019-03-23 12:55
  */
-public interface HttpServerInterface
+public abstract class HttpServer
 {
-    
+
+
+    /**
+     * 在当前线程启动HTTP服务器
+     *
+     * @throws HttpServerException 启动失败
+     */
+    public abstract void start() throws HttpServerException;
 }
