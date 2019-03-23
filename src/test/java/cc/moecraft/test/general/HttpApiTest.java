782f43320dc11e7b09986bfd4aced690ace07b5e
Hykilpikonna <admin@moecraft.cc>
2019-03-23 10:07:14 -0400

    [+] Create unit test

---
+++ src/test/java/cc/moecraft/test/general/HttpApiTest.java
@@ -0,0 +1,27 @@
+package cc.moecraft.test.general;
+
+import cc.moecraft.icq.PicqBotX;
+import cc.moecraft.icq.sender.IcqHttpApi;
+import cc.moecraft.logger.HyLogger;
+import cc.moecraft.test.icq.listeners.ExceptionListener;
+import cc.moecraft.test.icq.listeners.TestListener;
+import cn.hutool.core.thread.ThreadUtil;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ * The class {@code HttpApiTest} is the unit tester for the http api.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-21!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-21 21:05
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class HttpApiTest
+{
+}
