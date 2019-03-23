70329e0b64d5bcbe482c96b8e5701a1732c9b325
Hykilpikonna <admin@moecraft.cc>
2019-03-23 11:54:33 -0400

    [+] Create return pojo class: RBoolean

---
+++ src/main/java/cc/moecraft/icq/sender/returndata/returnpojo/get/RBoolean.java
@@ -0,0 +1,30 @@
+package cc.moecraft.icq.sender.returndata.returnpojo.get;
+
+import cc.moecraft.icq.sender.returndata.returnpojo.ReturnPojoBase;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+
+/**
+ * The class {@code RBoolean} is a return pojo for api interfaces with
+ * only one boolean value.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-23!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-23 11:09
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Setter(AccessLevel.NONE)
+public class RBoolean extends ReturnPojoBase
+{
+    @SerializedName("yes")
+    @Expose
+    public Boolean yes;
+}
