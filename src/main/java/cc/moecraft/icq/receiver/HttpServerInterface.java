4acc2ecc1726391a19355f114ff8116180207518
Hykilpikonna <admin@moecraft.cc>
2019-03-23 12:57:54 -0400

    [+] Create an interface for HttpServer.

---
+++ src/main/java/cc/moecraft/icq/receiver/HttpServerInterface.java
@@ -0,0 +1,17 @@
+package cc.moecraft.icq.receiver;
+
+/**
+ * The class {@code HttpServerInterface} is a http server to receive and
+ * parse the package sent from the http plugin of CoolQ.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-23!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-23 12:55
+ */
+public interface HttpServerInterface
+{
+    
+}
