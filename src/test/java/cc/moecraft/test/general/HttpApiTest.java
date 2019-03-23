ac13a961d364a589de9c80add51837f5d3f7f346
Hykilpikonna <admin@moecraft.cc>
2019-03-23 10:10:01 -0400

    [+] Create init() method to initialize and start bot

--- src/test/java/cc/moecraft/test/general/HttpApiTest.java
+++ src/test/java/cc/moecraft/test/general/HttpApiTest.java
@@ -28,4 +28,26 @@ public class HttpApiTest
     private static final long TEST_RECEIVE_GR = 491046707; // 用来测试的群
 
     private static PicqBotX bot;
+    @BeforeClass
+    public static void init()
+    {
+        // 创建机器人对象 ( 接收端口 )
+        PicqBotX bot = new PicqBotX(30192);
+
+        // 添加一个机器人账户 ( 名字, 发送URL, 发送端口 )
+        bot.addAccount("Bot01", "127.0.0.1", 31091);
+
+        // 注册事件
+        bot.getEventManager().registerListeners(new TestListener(), new ExceptionListener());
+
+        // 启用指令管理器, Unit test
+        bot.enableCommandManager(false, "ut -");
+
+        api = bot.getAccountManager().getNonAccountSpecifiedApi();
+        logger = bot.getLogger();
+
+        // 启动机器人
+        new Thread(bot::startBot).start();
+        ThreadUtil.safeSleep(2000);
+    }
 }
