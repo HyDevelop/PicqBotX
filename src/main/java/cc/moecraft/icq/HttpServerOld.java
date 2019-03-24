02adad28f2db950d04ba81f600f3a9e446f197c2
Hykilpikonna <admin@moecraft.cc>
2019-03-24 09:23:06 -0400

    [U] Implement changes in old http server class

--- src/main/java/cc/moecraft/icq/HttpServerOld.java
+++ src/main/java/cc/moecraft/icq/HttpServerOld.java
@@ -1,7 +1,7 @@
 package cc.moecraft.icq;
 
-import cc.moecraft.icq.event.events.local.EventLocalHttpFailEvent;
-import cc.moecraft.icq.event.events.local.EventLocalHttpReceiveEvent;
+import cc.moecraft.icq.event.events.local.EventLocalHttpFail;
+import cc.moecraft.icq.event.events.local.EventLocalHttpReceive;
 import cc.moecraft.icq.exceptions.HttpServerException;
 import cc.moecraft.logger.HyLogger;
 import cc.moecraft.logger.format.AnsiColor;
@@ -139,7 +139,7 @@ public class HttpServerOld
                 String line = reader.readLine();
                 if (line == null || line.isEmpty())
                 {
-                    bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.REQUEST_IS_EMPTY));
+                    bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.REQUEST_IS_EMPTY));
                     continue;
                 }
 
@@ -149,7 +149,7 @@ public class HttpServerOld
 
                 if (!method.equalsIgnoreCase("post"))
                 {
-                    bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.INCORRECT_REQUEST_METHOD));
+                    bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.INCORRECT_REQUEST_METHOD));
                     continue;
                 }
 
@@ -191,12 +191,12 @@ public class HttpServerOld
                 // 验证信息
                 if (contentType.equals("UNINITIALIZED") || !contentType.equals("application/json"))
                 {
-                    bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.INCORRECT_APPLICATION_TYPE));
+                    bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.INCORRECT_APPLICATION_TYPE));
                     continue;
                 }
                 if (charset.equals("UNINITIALIZED") || !charset.equals("charset=UTF-8"))
                 {
-                    bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.INCORRECT_CHARSET));
+                    bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.INCORRECT_CHARSET));
                     continue;
                 }
                 if (userAgent.equals("UNINITIALIZED") || !userAgent.matches(HTTP_API_VERSION_DETECTION))
@@ -206,7 +206,7 @@ public class HttpServerOld
                     logger.error("当前版本为: " + userAgent);
                     logger.error("推荐更新这个类库或者HTTP API的版本");
                     logger.error("如果要无视版本检查, 请在启动前加上 \"机器人对象.setHttpApiVersionDetection(\"*\");\"");
-                    bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.INCORRECT_VERSION));
+                    bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.INCORRECT_VERSION));
                     continue;
                 }
 
@@ -236,7 +236,7 @@ public class HttpServerOld
                     logger.debug("- 数据: " + data);
                 }
 
-                EventLocalHttpReceiveEvent event = new EventLocalHttpReceiveEvent(info, otherInfo, contentType, charset, userAgent, data);
+                EventLocalHttpReceive event = new EventLocalHttpReceive(info, otherInfo, contentType, charset, userAgent, data);
 
                 bot.getEventManager().call(event);
 
@@ -251,7 +251,7 @@ public class HttpServerOld
                 logger.error("变量: " + ExceptionUtils.getAllVariables(e));
                 ThreadUtils.safeSleep(2);
                 e.printStackTrace();
-                bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.UNKNOWN));
+                bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.UNKNOWN));
             }
             finally
             {
@@ -266,7 +266,7 @@ public class HttpServerOld
                 {
                     logger.error("关闭接收失败: ");
                     e.printStackTrace();
-                    bot.getEventManager().call(new EventLocalHttpFailEvent(EventLocalHttpFailEvent.Reason.SOCKET_CLOSE_FAILED));
+                    bot.getEventManager().call(new EventLocalHttpFail(EventLocalHttpFail.Reason.SOCKET_CLOSE_FAILED));
                 }
             }
         }
