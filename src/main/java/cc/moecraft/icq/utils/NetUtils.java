edc946b57179ca2ad73bbe8f8c5d60bcf0f382ca
Hykilpikonna <admin@moecraft.cc>
2019-03-24 12:32:11 -0400

    [+] Create utility class NetUtils

---
+++ src/main/java/cc/moecraft/icq/utils/NetUtils.java
@@ -0,0 +1,18 @@
+package cc.moecraft.icq.utils;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ * The class {@code NetUtils} is an utility class for Socket servers.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-23!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-23 13:54
+ */
+public class NetUtils
+{
+}
