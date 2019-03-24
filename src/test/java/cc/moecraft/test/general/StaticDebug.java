35203a58a72a9b8f2a89163b7cd1208c84ff9e66
Hykilpikonna <admin@moecraft.cc>
2019-03-24 14:14:18 -0400

    [T] Test commandArgs parsing

--- src/test/java/cc/moecraft/test/general/StaticDebug.java
+++ src/test/java/cc/moecraft/test/general/StaticDebug.java
@@ -1,15 +1,12 @@
 package cc.moecraft.test.general;
 
 import cc.moecraft.icq.command.CommandArgs;
+import cc.moecraft.icq.command.CommandManager;
 import cc.moecraft.icq.command.exceptions.CommandNotFoundException;
 import cc.moecraft.icq.command.exceptions.NotACommandException;
-import cc.moecraft.icq.command.interfaces.IcqCommand;
 import cc.moecraft.test.icq.commands.CommandTest;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import static cc.moecraft.icq.command.CommandArgsParser.parse;
 
 /**
  * 此类由 Hykilpikonna 在 2018/08/10 创建!
@@ -23,12 +20,10 @@ public class StaticDebug
 {
     public static void main(String[] args) throws NotACommandException, CommandNotFoundException
     {
-        String[] prefixes = new String[]{"!", "bot -"};
-        Map<String, ArrayList<IcqCommand>> registeredCommands = new HashMap<>();
-        CommandTest command = new CommandTest();
-        registeredCommands.put(command.properties().getName().toLowerCase(), new ArrayList<>(Collections.singletonList(command)));
+        CommandManager commandManager = new CommandManager("!", "bot -");
+        commandManager.registerCommand(new CommandTest());
 
-        CommandArgs commandArgs = CommandArgs.parse(prefixes, registeredCommands, "   !  test a b  cc d   ", true);
+        CommandArgs commandArgs = parse(commandManager, "   !  test a b  cc d   ", true);
         System.out.println(commandArgs);
     }
 }
