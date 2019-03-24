7016d92fdc6272d7e99e73f7c6bdc331435b30e8
Hykilpikonna <admin@moecraft.cc>
2019-03-24 13:07:41 -0400

    [+] Use lombok constructors instead for PicqConfig

--- src/main/java/cc/moecraft/icq/PicqConfig.java
+++ src/main/java/cc/moecraft/icq/PicqConfig.java
@@ -1,7 +1,9 @@
 package cc.moecraft.icq;
 
 import cc.moecraft.logger.environments.ColorSupportLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
 import static cc.moecraft.logger.environments.ColorSupportLevel.FORCED;
@@ -21,6 +23,8 @@ import static cc.moecraft.logger.environments.ColorSupportLevel.FORCED;
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2019-03-21 18:40
  */
+@AllArgsConstructor
+@RequiredArgsConstructor
 @Data
 @Accessors(chain = true)
 public class PicqConfig
@@ -29,89 +33,35 @@ public class PicqConfig
     private final int socketPort;
 
     /** 是否输出Debug消息 */
-    private boolean debug;
+    private boolean debug = true; //TODO: change this to false
 
     /** 是否跳过酷Q版本验证 (不推荐) */
-    private boolean noVerify;
+    private boolean noVerify = false;
 
     /** 是否启用异步 */
-    private boolean useAsync;
+    private boolean useAsync = false;
 
     /** 是否启用维护模式 */
-    private boolean maintenanceMode;
+    private boolean maintenanceMode = false;
 
     /** 是否开启多账号优化 */
-    private boolean multiAccountOptimizations;
+    private boolean multiAccountOptimizations = true;
 
     /** 是否暂停事件 */
-    private boolean eventPaused;
+    private boolean eventPaused = false;
 
     /** 是否暂停HTTP接收 */
-    private boolean httpPaused;
+    private boolean httpPaused = false;
 
     // 一次配置项 | Unchangeable config fields
     // 这里的配置init之后就不能变了w
 
     /** Logger颜色支持级别 */
-    private ColorSupportLevel colorSupportLevel;
+    private ColorSupportLevel colorSupportLevel = FORCED;
 
     /** Logger日志路径 */
-    private String logPath;
+    private String logPath = "logs";
 
     /** Logger日志文件名 */
-    private String logFileName;
-
-    /**
-     * 全参数构造器
-     *
-     * @param socketPort 接收端口
-     * @param debug 是否输出Debug消息
-     * @param noVerify 是否跳过酷Q版本验证 (不推荐)
-     * @param useAsync 是否启用异步
-     * @param maintenanceMode 是否启用维护模式
-     * @param multiAccountOptimizations 是否开启多账号优化
-     * @param eventPaused 是否暂停事件
-     * @param httpPaused 是否暂停HTTP接收
-     * @param colorSupportLevel Logger颜色支持级别
-     * @param logPath Logger日志路径
-     * @param logFileName Logger日志文件名
-     */
-    public PicqConfig(int socketPort, boolean debug, boolean noVerify, boolean useAsync, boolean maintenanceMode,
-                      boolean multiAccountOptimizations, boolean eventPaused, boolean httpPaused,
-                      ColorSupportLevel colorSupportLevel, String logPath, String logFileName)
-    {
-        this.socketPort = socketPort;
-        this.debug = debug;
-        this.noVerify = noVerify;
-        this.useAsync = useAsync;
-        this.maintenanceMode = maintenanceMode;
-        this.multiAccountOptimizations = multiAccountOptimizations;
-        this.eventPaused = eventPaused;
-        this.httpPaused = httpPaused;
-
-        this.colorSupportLevel = colorSupportLevel;
-        this.logPath = logPath;
-        this.logFileName = logFileName;
-    }
-
-    /**
-     * 必要参数构造器
-     *
-     * @param socketPort 接收端口
-     */
-    public PicqConfig(int socketPort)
-    {
-        this.socketPort = socketPort;
-        this.debug = true; //TODO: change this to false
-        this.noVerify = false;
-        this.useAsync = false;
-        this.maintenanceMode = false;
-        this.multiAccountOptimizations = true;
-        this.eventPaused = false;
-        this.httpPaused = false;
-
-        this.colorSupportLevel = FORCED;
-        this.logPath = "logs";
-        this.logFileName = "PicqBotX-Log";
-    }
+    private String logFileName = "PicqBotX-Log";
 }
