a28012c7884d769303c2df7813f1840771bac893
Hykilpikonna <admin@moecraft.cc>
2019-03-24 14:32:31 -0400

    [U] Testing Release v3.0.2.755

--- src/main/java/cc/moecraft/icq/PicqConstants.java
+++ src/main/java/cc/moecraft/icq/PicqConstants.java
@@ -18,7 +18,7 @@ public class PicqConstants
     // 版本常量 | Version constant
 
     /** 当前版本 */
-    public static final String VERSION = "3.0.1.730";
+    public static final String VERSION = "3.0.2.755";
 
     /** 兼容版本检测Regex */
     public static final String HTTP_API_VERSION_DETECTION = ".*4.8.*";
