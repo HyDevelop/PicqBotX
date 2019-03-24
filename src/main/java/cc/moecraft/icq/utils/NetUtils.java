2a5abe8f6b0893a0755479402e88221d015998e3
Hykilpikonna <admin@moecraft.cc>
2019-03-24 12:32:26 -0400

    [+] Create utility method to read from input stream

--- src/main/java/cc/moecraft/icq/utils/NetUtils.java
+++ src/main/java/cc/moecraft/icq/utils/NetUtils.java
@@ -15,4 +15,15 @@ import java.util.Scanner;
  */
 public class NetUtils
 {
+    /**
+     * 读取字符串数据
+     *
+     * @param in 输入流
+     * @return 字符串
+     */
+    public static String read(InputStream in)
+    {
+        Scanner scanner = new Scanner(in, "UTF-8").useDelimiter("\\A");
+        return scanner.hasNext() ? scanner.next() : "";
+    }
 }
