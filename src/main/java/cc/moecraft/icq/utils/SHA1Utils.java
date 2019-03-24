2edbdd192b9493215f43afc4690c217477d48578
Hykilpikonna <admin@moecraft.cc>
2019-03-24 17:06:14 -0400

    [+] Create class SHA1Utils

---
+++ src/main/java/cc/moecraft/icq/utils/SHA1Utils.java
@@ -0,0 +1,20 @@
+package cc.moecraft.icq.utils;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Formatter;
+
+/**
+ * The class {@code SHA1Utils} is a utility class to generate SHA1 hashes
+ * and validate them.
+ * <p>
+ * Class created by the HyDEV Team on 2019-03-24!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2019-03-24 16:38
+ */
+public class SHA1Utils
+{
+}
