39be8dfb7f1a4fd546eb60f5c3e5522a92c09986
Hykilpikonna <admin@moecraft.cc>
2019-03-25 08:01:28 -0400

    [T] Run unit test with limited rate

--- src/test/java/cc/moecraft/test/general/HttpApiTest.java
+++ src/test/java/cc/moecraft/test/general/HttpApiTest.java
@@ -40,7 +40,8 @@ public class HttpApiTest
                 .setDebug(true)
                 .setSecret("This is secret")
                 .setAccessToken("Brq4KSm+3UdaUJnLZ+AJfj**v-vePWL$")
-                .setUseAsync(true)
+                //.setUseAsync(true)
+                .setApiRateLimited(true)
         );
 
         // 添加一个机器人账户 ( 名字, 发送URL, 发送端口 )
