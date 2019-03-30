a68066569f6484ffeb2a6c6bd4b6b18f7ece3ecb
Hykilpikonna <admin@moecraft.cc>
2019-03-30 14:56:11 -0400

    [T] Fix bot instance variable is null

--- src/test/java/cc/moecraft/test/general/HttpApiTest.java
+++ src/test/java/cc/moecraft/test/general/HttpApiTest.java
@@ -37,12 +37,12 @@ public class HttpApiTest
     public static void init()
     {
         // 创建机器人对象 ( 接收端口 )
-        PicqBotX bot = new PicqBotX(new PicqConfig(31092)
+        bot = new PicqBotX(new PicqConfig(31092)
                 .setDebug(true)
                 .setSecret("This is secret")
                 .setAccessToken("Brq4KSm+3UdaUJnLZ+AJfj**v-vePWL$")
                 //.setUseAsync(true)
-                .setApiRateLimited(true)
+                //.setApiRateLimited(true)
         );
 
         // 添加一个机器人账户 ( 名字, 发送URL, 发送端口 )
