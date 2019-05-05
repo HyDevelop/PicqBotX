35c4118a806aec02165ce236fa06334dceddd949
Hykilpikonna <admin@moecraft.cc>
2019-05-05 15:07:50 -0400

    [O] Optimize javadocs

--- src/main/java/cc/moecraft/icq/event/Event.java
+++ src/main/java/cc/moecraft/icq/event/Event.java
@@ -11,10 +11,7 @@ import lombok.Data;
 import lombok.Setter;
 
 /**
- * 此类由 Hykilpikonna 在 2018/05/24 创建!
- * Created by Hykilpikonna on 2018/05/24!
- * Github: https://github.com/hykilpikonna
- * Meow!
+ * 事件
  *
  * @author Hykilpikonna
  */
