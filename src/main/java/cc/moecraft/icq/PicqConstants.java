b03c4435b60bc4a9552136c2d3665b5075684411
Hykilpikonna <admin@moecraft.cc>
2019-05-05 14:43:14 -0400

    [U] Update CQHTTP version detection

--- src/main/java/cc/moecraft/icq/PicqConstants.java
+++ src/main/java/cc/moecraft/icq/PicqConstants.java
@@ -21,7 +21,7 @@ public class PicqConstants
     public static final String VERSION = "3.2.2.896";
 
     /** 兼容版本检测Regex */
-    public static String HTTP_API_VERSION_DETECTION = ".*4.8.*";
+    public static String HTTP_API_VERSION_DETECTION = ".*4.10.*";
 
 
     // 可变常量 | Variable constant
