40307a4fd449dcbac704a46c68528c5376be059a
Hykilpikonna <admin@moecraft.cc>
2019-10-12 13:50:54 -0400

    [U] Release v4.11.0.971

--- src/main/java/cc/moecraft/icq/PicqConstants.java
+++ src/main/java/cc/moecraft/icq/PicqConstants.java
@@ -18,7 +18,7 @@ public class PicqConstants
     // 版本常量 | Version constant
 
     /** 当前版本 */
-    public static final String VERSION = "4.10.1.928";
+    public static final String VERSION = "4.11.0.971";
 
     /** 兼容版本检测Regex */
     public static String HTTP_API_VERSION_DETECTION = ".*4.10.*";
