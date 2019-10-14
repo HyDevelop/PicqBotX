d5b94f3907e0870cc5e8df4075f8051f796091c4
Hykilpikonna <admin@moecraft.cc>
2019-10-14 15:32:29 -0400

    [U] Pre-Release v4.12.0.989.PRE

--- src/main/java/cc/moecraft/icq/PicqConstants.java
+++ src/main/java/cc/moecraft/icq/PicqConstants.java
@@ -18,7 +18,7 @@ public class PicqConstants
     // 版本常量 | Version constant
 
     /** 当前版本 */
-    public static final String VERSION = "4.11.0.971";
+    public static final String VERSION = "4.12.0.989.PRE";
 
     /** 兼容版本检测Regex */
     public static String HTTP_API_VERSION_DETECTION = ".*4.10.*";
