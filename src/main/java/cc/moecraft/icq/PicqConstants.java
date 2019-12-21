c1e01ebb42383542102d683f528cb08f5d2c1e51
Hykilpikonna <admin@moecraft.cc>
2019-12-20 23:05:00 -0500

    [F] Fix version numbers

--- src/main/java/cc/moecraft/icq/PicqConstants.java
+++ src/main/java/cc/moecraft/icq/PicqConstants.java
@@ -18,7 +18,7 @@ public class PicqConstants
     // 版本常量 | Version constant
 
     /** 当前版本 */
-    public static final String VERSION = "4.12.0.991.PRE";
+    public static final String VERSION = "4.12.0.1015.PRE";
 
     /** 兼容版本检测Regex */
     public static String HTTP_API_VERSION_DETECTION = ".*4.12.*";
